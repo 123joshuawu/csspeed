f0e881f07caac827b4623cb8a500dadd314380ae
thestopsign <ethanwhitton@snowmobility.com>
2020-12-08 22:52:35 -0500

    init landing index and play page

---
+++ index.js
@@ -0,0 +1,9 @@
+function loadLanding() {
+    const scores = localStorage.getItem("scores")
+    console.log("Scores:",scores)
+}
+
+function loadPlay() {
+    const scores = localStorage.getItem("scores")
+    console.log("Scores:",scores)
+}
